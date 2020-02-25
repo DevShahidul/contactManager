0e3965f28d1b27967ca5851f4388617360d933fb
Shahidul Islam <ishahin56@gmail.com>
2020-02-25 13:22:09 +0600

    Updates

---
+++ precache-manifest.a12b7560462c6d130cf1a37990defa96.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04c86288c311833fe385b69f68eec731",
+    "url": "/contactManager/index.html"
+  },
+  {
+    "revision": "2596765f7f3eee04e07a",
+    "url": "/contactManager/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "59b7e87ad8ce8c29f1d9",
+    "url": "/contactManager/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "2596765f7f3eee04e07a",
+    "url": "/contactManager/static/js/2.6453af59.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/contactManager/static/js/2.6453af59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59b7e87ad8ce8c29f1d9",
+    "url": "/contactManager/static/js/main.cc201aa5.chunk.js"
+  },
+  {
+    "revision": "3b51a892d36901a6587f",
+    "url": "/contactManager/static/js/runtime-main.90107759.js"
+  }
+]);
